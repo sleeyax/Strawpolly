ac41415c32766d1f4d46ba18080e7184bfd5b1f4
Sleeyax <yourd3veloper@gmail.com>
2019-10-10 16:01:07 +0200

    Show amount of members

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private memeberCount: number;
+
+  constructor(private api: ApiService) {
+    this.api.getMembers().subscribe((users) => {
+      this.memeberCount = users.length;
+    })
+  }
 
   ngOnInit() {
   }
