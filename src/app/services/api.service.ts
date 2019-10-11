6c9f6029755ccbb0b3700b1e84ebfc2ebe4fc12a
Sleeyax <yourd3veloper@gmail.com>
2019-10-11 23:13:19 +0200

    Add authentication headers

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
 import {apiEndpoint} from '../config.json';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import User from '../models/user';
+import {StorageService} from './storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,26 +11,51 @@ import User from '../models/user';
 export class ApiService {
   private api: string = `${apiEndpoint}/api`;
   private readonly membersResource: string = '/members';
+  private headers: HttpHeaders = new HttpHeaders();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private storage: StorageService) {}
+
+  private setAuthenticationHeaders() {
+    if (this.storage.token != null)
+      this.headers = this.headers.set("Authorization", "Bearer " + this.storage.token);
+  }
+
+  /**
+   * Log user in
+   * @param user
+   */
+  public authenticate(user: {email: string, password: string}) {
+    return this.sendPost(`${this.membersResource}/authenticate`, user);
+  }
 
   /**
    * Sends a GET request to the specified resource
    * @param resource
+   * @param options
    */
-  private sendGet<T>(resource: string) {
-    return this.http.get<T>(`${this.api}${resource}`);
+  private sendGet<T>(resource: string, options?: {auth: boolean}) {
+    if (options && options.auth)
+      this.setAuthenticationHeaders();
+    return this.http.get<T>(`${this.api}${resource}`, {headers: this.headers});
   }
 
-  private sendPost<T>(resource: string, data: Object) {
-    return this.http.post(`${this.api}${resource}`, data);
+  /**
+   * Sends a POST request to the specified resource
+   * @param resource
+   * @param data
+   * @param options
+   */
+  private sendPost<T>(resource: string, data: Object, options?: {auth: boolean}) {
+    if (options && options.auth)
+      this.setAuthenticationHeaders();
+    return this.http.post(`${this.api}${resource}`, data, {headers: this.headers});
   }
 
   /**
    * Returns all registered members
    */
   public getMembers() : Observable<User[]> {
-    return this.sendGet<User[]>(this.membersResource);
+    return this.sendGet<User[]>(this.membersResource, {auth: true});
   }
 
   /**
