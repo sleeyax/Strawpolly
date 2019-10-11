4f25005cec43937d8f539370f82784135c1ee088
Sleeyax <yourd3veloper@gmail.com>
2019-10-11 23:28:53 +0200

    Fix test

--- src/app/services/interceptors/security.interceptor.service.spec.ts
+++ src/app/services/interceptors/security.interceptor.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Security.InterceptorService } from './security.interceptor.service';
+import { SecurityInterceptor } from './security.interceptor.service';
 
 describe('Security.InterceptorService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: Security.InterceptorService = TestBed.get(Security.InterceptorService);
+    const service: SecurityInterceptor = TestBed.get(SecurityInterceptor);
     expect(service).toBeTruthy();
   });
 });
