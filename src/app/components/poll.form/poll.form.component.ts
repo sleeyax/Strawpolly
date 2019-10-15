f946c7a9b1afd86f0208e9e6e54496916f055443
Sleeyax <yourd3veloper@gmail.com>
2019-10-15 20:55:42 +0200

    Submit form

--- src/app/components/poll.form/poll.form.component.ts
+++ src/app/components/poll.form/poll.form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormArray, FormBuilder, Validators} from '@angular/forms';
+import {Answer, Poll} from '../../models/poll';
+import {ApiService} from '../../services/api.service';
 
 @Component({
   selector: 'app-poll.form',
@@ -13,13 +15,28 @@ export class PollFormComponent implements OnInit {
     fields: this.fb.array([this.createField()])
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private api: ApiService) {}
 
   ngOnInit() {
   }
 
   onFormSubmit() {
-    // TODO: submit form
+    // TODO: check if form is valid
+
+    console.log(this.pollForm.value);
+
+    const answers = this.pollForm.value.fields
+      .filter((field) => field != "")
+      .map((field) => new Answer(field));
+
+    const poll = new Poll(this.pollForm.value.title, answers);
+    console.log(poll);
+
+    // TODO: catch errors and re-route to dashboard if success
+    this.api.createPoll(poll).subscribe(
+      (response) => console.log(response),
+      (err) => console.log(err)
+    );
   }
 
   /**
