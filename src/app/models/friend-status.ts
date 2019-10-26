1e2dc21a89445d796e667b5ada8bda6dd1c717ce
Sleeyax <yourd3veloper@gmail.com>
2019-10-26 13:42:56 +0200

    Add 'Removed' value

--- src/app/models/friend-status.ts
+++ src/app/models/friend-status.ts
@@ -3,7 +3,9 @@ enum FriendStatus
   Pending,
   Accepted,
   Declined,
-  Blocked
+  Blocked,
+  // NOTE: the value below is only used to keep the code clean. It's not stored in the database; removed values == removed rows
+  Removed
 }
 
 export default FriendStatus;
